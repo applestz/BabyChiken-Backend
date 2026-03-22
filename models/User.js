884d4525fcc709b966bbcbc0b0236b52b4314846
wateronenn <anamneung@gmail.com>
2026-03-22 17:49:44 +0700

    edit user update8

--- models/User.js
+++ models/User.js
@@ -57,7 +57,9 @@ const UserSchema = new mongoose.Schema({
 
 //Encrypt password using bcrypt
 UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next(); 
+  if (!this.isModified('password')) {
+    return;
+  } 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
